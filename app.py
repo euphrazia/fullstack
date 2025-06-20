2f4b913ffe8870768ac9cca6128a4cf7e66fe0a7
euphrazia <euphraziaomondi516@gmail.com>
2025-06-20 21:21:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -108,6 +108,10 @@ def register():
     return render_template('register.html', error=error)
 
 
+
+    
+    import os
+
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
